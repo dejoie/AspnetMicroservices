c630d69a0de283f9ef29c3ef7751755115cc453c
pierre-andre dejoie <pierre-andre.dejoie@aginsurance.be>
2023-04-08 07:13:00 +0200

    latest changes

---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,26 @@
+﻿using Discount.Grpc.Protos;
+using System;
+using System.Net.Http;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoService;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoService)
+        {
+            _discountProtoService = discountProtoService ?? throw new ArgumentNullException(nameof(discountProtoService));
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productName };
+
+            HttpClient.DefaultProxy = new WebProxy();
+            return await _discountProtoService.GetDiscountAsync(discountRequest);
+        }
+    }
+}
